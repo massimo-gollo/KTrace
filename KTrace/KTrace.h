358889fee5d1a6348e97ba7f9e56740316755204
Massimo Gollo <m.gollo@sangiorgisrl.it>
2022-03-10 23:47:09 +0100

    Initial Commit

---
+++ KTrace/KTrace.h
@@ -0,0 +1,18 @@
+//
+//  KTrace.h
+//  KTrace
+//
+//  Created by Massimo Gollo on 10/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KTrace.
+FOUNDATION_EXPORT double KTraceVersionNumber;
+
+//! Project version string for KTrace.
+FOUNDATION_EXPORT const unsigned char KTraceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KTrace/PublicHeader.h>
+
+
