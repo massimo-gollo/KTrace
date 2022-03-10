18eee135fcd3b8aba4e381c0805a8b7690b6124b
Massimo Gollo <m.gollo@sangiorgisrl.it>
2022-03-10 23:52:52 +0100

    Initial project setup

---
+++ KTracerExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KTracerExample
+//
+//  Created by Massimo Gollo on 10/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KTracerExample/KTracerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KTracerExampleApp.swift
+//  KTracerExample
+//
+//  Created by Massimo Gollo on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct KTracerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
